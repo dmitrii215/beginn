353a689f022edd6c4fa039bd9ba29f31c0b8f54d
dmitrii215 <stepanovda@bk.ru>
2024-11-16 01:23:46 +0300

    бот начало

---
+++ Module13.3/module_13_3.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=["start"])
+async def start_message(message):
+    text = "Привет! Я бот помогающий твоему здоровью."
+    await message.answer(text)
+    print(text)
+
+
+@dp.message_handler()
+async def all_message(message):
+    text = "Ведите команду /start чтобы начать общение"
+    await message.answer(text)
+    print(text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
